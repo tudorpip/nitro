cd77c312f9dd45a9dccffc05f39414618858f40c
Tudor Pipernea <T.Pipernea@student.tudelft.nl>
2024-07-24 15:21:10 +0200

    Update for the auto-generation of the yaml files

--- src/presets/genezio/utils.ts
+++ src/presets/genezio/utils.ts
@@ -11,15 +11,25 @@ export async function generateFunctionFiles(nitro: Nitro) {
   # The version of the Genezio YAML configuration to parse.
   yamlVersion: 2
   backend:
+    # The root directory of the backend.
     path: .output/
+    # Information about the backend's programming language.
     language:
+        # The name of the programming language.
         name: js
+        # The package manager used by the backend.
         packageManager: npm
+    # Information about the backend's functions.    
     functions:
+        # The name (label) of the function.
         - name: nitroServer
+        # The path to the function's code.
           path: server/
+          # The name of the function handler
           handler: handler
+          # The entry point for the function.
           entry: index.mjs
+          # The compatibility of the function handler.
           type: aws
   `.trim();
   try {
